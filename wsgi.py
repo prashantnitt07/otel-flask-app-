92dbd3816e7fec48fbb8dab836e1e26b4dba0e6d
prashantnitt07 <31304206+prashantnitt07@users.noreply.github.com>
2025-11-09 23:12:30 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
-from app import app, prometheus_reader
-from opentelemetry.exporter.prometheus import start_http_server
+from app import app
 
-# The Prometheus exporter opens its own HTTP endpoint.
-# Prometheus scrapes this endpoint, not Flask's 7000.
-METRICS_PORT = 9464
-start_http_server(port=METRICS_PORT)
-
+# Gunicorn looks for 'application'
 application = app
