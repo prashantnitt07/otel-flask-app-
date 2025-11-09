60c2ece8864c91eb544cce85c7289552bb229690
prashantnitt07 <31304206+prashantnitt07@users.noreply.github.com>
2025-11-09 23:31:30 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app as application
 
-# Gunicorn looks for 'application'
-application = app
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=7000)
